f4ba8aa3507ebda1413dacc8d90be3a5466592ca
Charaf Oualid <542269@student.fontys.nl>
2026-05-19 12:18:26 +0200

    Initial project setup

---
+++ services/database.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+const { MongoClient } = require("mongodb");
+
+const uri = process.env.MONGO_URL;
+
+const client = new MongoClient(uri);
+
+const db = client.db(process.env.DB_NAME || "mydb");
+
+module.exports = {
+  db: db,
+  client: client
+};
