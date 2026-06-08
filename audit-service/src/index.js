173bffdd8211d827f20afb3c38a0caf31f18876f
Charaf Oualid <542269@student.fontys.nl>
2026-06-08 15:51:39 +0200

    audit service

---
+++ audit-service/src/index.js
@@ -0,0 +1,36 @@
+const amqp = require("amqplib");
+const { MongoClient } = require("mongodb");
+
+async function start() {
+  const mongoClient = new MongoClient(process.env.AUDIT_MONGO_URL);
+  await mongoClient.connect();
+
+  const db = mongoClient.db(process.env.AUDIT_DB_NAME);
+  const auditLogs = db.collection("auditlogs");
+
+  const connection = await amqp.connect(process.env.RABBITMQ_URL);
+  const channel = await connection.createChannel();
+
+  const queue = "user.created";
+
+  await channel.assertQueue(queue, { durable: true });
+
+  console.log("Audit service luistert naar user.created");
+
+  channel.consume(queue, async (message) => {
+    const data = JSON.parse(message.content.toString());
+
+    await auditLogs.insertOne({
+      event: data.event,
+      user: data.user,
+      userId: data.userId,
+      createdAt: new Date()
+    });
+
+    console.log("Audit event opgeslagen:", data.event);
+
+    channel.ack(message);
+  });
+}
+
+start().catch(console.error);
