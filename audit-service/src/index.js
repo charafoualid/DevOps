2392d56be0efa23c3571f63b16fa7a53b7e052db
Charaf Oualid <542269@student.fontys.nl>
2026-06-18 22:51:31 +0200

    gecomment

--- audit-service/src/index.js
+++ audit-service/src/index.js
@@ -1,7 +1,7 @@
 const amqp = require("amqplib");
 const { MongoClient } = require("mongodb");
 
-const testLint = "pipeline moet falen";
+//const testLint = "pipeline moet falen";
 
 async function start() {
   const mongoClient = new MongoClient(process.env.AUDIT_MONGO_URL);
